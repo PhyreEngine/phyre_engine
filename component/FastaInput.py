1ef823144525bda29841dd89c8b8d92a15935acc
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2016-10-08 22:03:11 +0100

    Add module for reading a FASTA file.

---
+++ component/FastaInput.py
@@ -0,0 +1,42 @@
+from component import Component
+from data.Sequence import Sequence
+
+class FastaInput(Component):
+    """Read a FASTA file as input and output the sequence."""
+
+    def run(self, input: str):
+        """Read the sequence from a FASTA file.
+
+        Reads a single sequence from the file with the path given by input.
+        If the file can not be read or contains multiple sequences an exception
+        will be thrown.
+
+        Args:
+            input: Path of the FASTA file to read.
+
+        Returns:
+            A Sequence object representing the sequence from the FASTA file.
+
+        Raises:
+            IOError: Error reading the file.
+            FastaInput.TooManySequencesError: The FASTA file contained multiple sequences.
+        """
+
+        with open(input, "r") as fasta:
+            #Count the number of identifiers so we can throw an errror if
+            #there is more than one.
+            identifiers = 0
+            seq_lines = []
+            for line in fasta:
+                if line.startswith(">"):
+                    identifiers = identifiers + 1
+                    if identifiers > 1:
+                        raise self.TooManySequencesError()
+                else:
+                    seq_lines.append(line.strip())
+            return Sequence("".join(seq_lines))
+
+
+    class TooManySequencesError(Exception):
+        """Indicates too many sequences were present in a FASTA file."""
+        pass
---
+++ test/component/test_FastaInput.py
@@ -0,0 +1,53 @@
+import unittest
+import tempfile
+import textwrap
+from component.FastaInput import FastaInput;
+
+class TestFastaInput(unittest.TestCase):
+
+    def test_valid(self):
+        """Create a valid FASTA file and read it back."""
+        seq_lines = [
+            'AYIAKQRQISFVKSHFSRQLEERLGLIEVQAPILSRVGDGTQDNL',
+            'SGAEKAVQVKVKALPDAQFEVVHSLAKWKRQTLGQHDFSAGEGLY',
+            'THMKALRPDEDRLSPLHSVYVDQWDWERVMGDGERQFSTLKSTVE',
+            'AIWAGIKATEAAVSEEFGLAPFLPDQIHFVHSQELLSRYPDLDAK',
+            'GRERAIAKDLGAVFLVGIGGKLSDGHRHDVRAPDYDDWSTPSELG',
+            'HAGLNGDILVWNPVLEDAFELSSMGIRVDADTLKHQLALTGDEDR',
+            'LELEWHQALLRGEMPQTIGGGIGQSRLTMLLLQLPHIGQVQAGVW',
+            'PAAVRESVPSLL']
+
+        fasta = ">An arbitrary identifer\n{}".format("\n".join(seq_lines))
+
+        valid_fasta_file = tempfile.NamedTemporaryFile("w")
+        valid_fasta_file.write(fasta)
+        valid_fasta_file.flush()
+
+
+        fasta_input = FastaInput()
+        seq = fasta_input.run(input=valid_fasta_file.name)
+        self.assertEqual(str(seq), "".join(seq_lines))
+
+    def test_file_exists(self):
+        """Check that an IOError is raised if the input file does not exist."""
+        with self.assertRaises(IOError):
+            FastaInput().run(input="bad_file_name")
+
+    def test_single_seq_required(self):
+        """Require a single sequence only."""
+
+        #Create a multi-sequence FASTA file
+        fasta = textwrap.dedent("""\
+                >ID 1
+                AYIAKQRQISFVKSHFSRQLEERLGLIEVQAPILSRVGDGTQDNL
+                >ID 2
+                SGAEKAVQVKVKALPDAQFEVVHSLAKWKRQTLGQHDFSAGEGLY
+                """)
+
+
+        msa_fasta_file = tempfile.NamedTemporaryFile("w")
+        msa_fasta_file.write(fasta)
+        msa_fasta_file.flush()
+
+        with self.assertRaises(FastaInput.TooManySequencesError):
+            FastaInput().run(input=msa_fasta_file.name)
