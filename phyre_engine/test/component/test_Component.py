fd5db40682b11bd4751b19dc04ed43c3d179a2a4
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2016-10-13 13:00:08 +0100

    Components should inherit from ABC Component.
    Also implemented a utility function "get_vals" in the Component base
    class that enables subclasses to easily get all elements in the REQUIRED
    list.

---
+++ phyre_engine/test/component/test_Component.py
@@ -0,0 +1,23 @@
+import unittest
+from phyre_engine.component import Component
+
+class TestComponent(unittest.TestCase):
+    """Test non-abstract methods in the Component abstract base class."""
+
+    class MockComponentScalar(Component):
+        REQUIRED = ["a"]
+        def run(data): pass
+
+    class MockComponentList(Component):
+        REQUIRED = ["a", "b"]
+        def run(data): pass
+
+    def test_get_vals(self):
+        data = {"a":123, "b":456}
+
+        a = TestComponent.MockComponentScalar().get_vals(data)
+        self.assertEqual(a, 123)
+
+        a, b = TestComponent.MockComponentList().get_vals(data)
+        self.assertEqual(a, 123)
+        self.assertEqual(b, 456)
