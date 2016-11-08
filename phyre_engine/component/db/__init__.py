fc7ad69cfbdb065f598e81af185299efa747ac33
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2016-11-08 15:48:35 +0000

    Add component for parsing RCSB-like cluster files
    Adds the ClusterParser class, which is responsible for parsing cluster
    files of the form used by the RCSB.

--- phyre_engine/component/db/__init__.py
+++ phyre_engine/component/db/__init__.py
@@ -64,3 +64,38 @@ class RCSBClusterDownload(Component):
                 self.filename)
         data["cluster_file"] = clus_file
         return data
+
+class ClusterParser(Component):
+    """Parse a cluster file.
+
+    The following keys are required when running this component.
+    ``cluster_file``:
+        File containing clusters. Each line of this file should represent one
+        cluster, with each structure in that cluster separated by whitespace.
+
+
+    The following keys are added when running this component:
+    ``clusters``:
+        Array of arrays containing PDB identifiers. Each sub-array is a
+        cluster, with the ordering preserved from the original file. PDB IDs
+        are of the form ``1XYZ_A``; that is, the PDB ID and chain ID separated
+        by an underscore.
+    """
+    REQUIRED = ['cluster_file']
+    ADDS     = ['clusters']
+    REMOVES  = []
+
+    def run(self, data):
+        """Download and parse the cluster file.
+
+        Args:
+            ``data``: Data carried through the pipeline.
+        """
+        clus_file = self.get_vals(data)
+
+        clusters = []
+        with open(clus_file) as clus_fh:
+            for clus_ln in clus_fh:
+                clusters.append(clus_ln.rstrip().split())
+        data["clusters"] = clusters
+        return data
--- phyre_engine/test/component/test_db.py
+++ phyre_engine/test/component/test_db.py
@@ -32,3 +32,37 @@ class TestRCSBClusterDownload(unittest.TestCase):
     def test_requires_valid_thresh(self):
         with self.assertRaises(ValueError, msg="Requires valid threshold"):
             db.RCSBClusterDownload(12)
+
+class TestClusterParser(unittest.TestCase):
+    """Test parsing a cluster file."""
+
+    def test_parse(self):
+        """Try and parse a dummy file."""
+
+        file_contents = (
+            "3J34_5 3J34_6 3J34_7 3J34_A 3J34_B 3J34_C 3J34_D \n"
+            "1CE6_B 4X6C_B 4X6C_D 4X6E_B \n"
+            "1LSG_A 5JEN_B 5JEN_D\n"
+            "2KXK_A 2LGB_A \n"
+            "4LCD_E \n"
+        )
+
+        with tempfile.NamedTemporaryFile(mode="w") as clus_file:
+            clus_file.write(file_contents)
+            clus_file.flush()
+
+            clus_parser = db.ClusterParser()
+            results = clus_parser.run({"cluster_file": clus_file.name})
+
+            self.assertIn("clusters", results, "Added 'clusters' key")
+
+            clusters = results["clusters"]
+            self.assertGreater(len(clusters), 0, "Got more than one cluster")
+            self.assertListEqual(
+                    clusters[0],
+                    "3J34_5 3J34_6 3J34_7 3J34_A 3J34_B 3J34_C 3J34_D".split(),
+                    "Cluster 0 correct")
+            self.assertListEqual(
+                    clusters[4],
+                    ["4LCD_E"],
+                    "Cluster 4 correct")
