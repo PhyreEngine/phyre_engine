d1d6cfab754fb7b79145bc3e8b0b28e2a386c3a7
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-04-07 15:51:48 +0100

    Add tool for side-chain angles and rotamers
    This commit adds a module "phyre_engine.tools.rotamer" with classes for
    representing side-chains and rotamers. The ".Sidechain" class contains
    methods for calculating χ angles, and the ".Rotamer" class contains
    methods for finding the rotameric state of a sidechain.

---
+++ phyre_engine/test/tools/test_rotamer.py
@@ -0,0 +1,159 @@
+import io
+import os
+import unittest
+from pathlib import Path
+import tempfile
+import Bio.PDB
+
+import phyre_engine.test
+import phyre_engine.tools.rotamer as rot
+
+DATA_DIR = Path(phyre_engine.test.__file__).parent / "data"
+DATA_DIR_2 = os.path.join(os.path.dirname(phyre_engine.test.__file__), 'data')
+
+class TestSidechain(unittest.TestCase):
+    """Test parsing of a single side-chain."""
+
+    VALINE = """
+ATOM     94  N   VAL A   1      22.212  26.467  28.766  1.00  9.92           N
+ATOM     95  CA  VAL A   1      22.047  25.268  27.956  1.00  6.81           C
+ATOM     96  C   VAL A   1      23.313  24.909  27.197  1.00 10.24           C
+ATOM     97  O   VAL A   1      23.823  23.796  27.329  1.00 23.59           O
+ATOM     98  CB  VAL A   1      20.888  25.446  26.949  1.00 10.72           C
+ATOM     99  CG1 VAL A   1      20.769  24.220  26.049  1.00  8.13           C
+ATOM    100  CG2 VAL A   1      19.597  25.667  27.701  1.00  2.66           C
+"""
+
+    LYSINE = """
+ATOM     31  N   LYS A   1      15.825  35.211  28.535  1.00  9.25           N
+ATOM     32  CA  LYS A   1      16.137  34.313  27.425  1.00  4.96           C
+ATOM     33  C   LYS A   1      17.626  34.197  27.104  1.00 11.71           C
+ATOM     34  O   LYS A   1      18.101  33.107  26.798  1.00  2.00           O
+ATOM     35  CB  LYS A   1      15.384  34.759  26.166  1.00  9.89           C
+ATOM     36  CG  LYS A   1      14.966  33.618  25.258  1.00 12.74           C
+ATOM     37  CD  LYS A   1      14.121  34.127  24.100  1.00  9.17           C
+ATOM     38  CE  LYS A   1      14.967  34.939  23.141  1.00  6.88           C
+ATOM     39  NZ  LYS A   1      14.138  35.863  22.307  1.00 17.64           N
+"""
+
+    def test_valine(self):
+        """Parse an atom with a single χ angle."""
+        with io.StringIO(self.VALINE) as string_fh:
+            pdb = Bio.PDB.PDBParser().get_structure("val", string_fh)
+            res = list(pdb.get_residues())[0]
+            angles = rot.Sidechain.calculate_angles(res)
+
+            # Instantiate objects and compare them
+            sc1 = rot.Sidechain("VAL", angles)
+            sc2 = rot.Sidechain.calculate(res)
+            self.assertTrue(sc1.isclose(sc2), "Instantiated side-chains match")
+
+            # Test angle parsing
+            self.assertEqual(len(angles), 1, "Valine has 1 χ angle")
+            self.assertAlmostEqual(angles[0], 176.8, 1, "Parsed χ1 of Valine")
+
+    def test_lysine(self):
+        """Parse an atom with four χ angles."""
+        with io.StringIO(self.LYSINE) as string_fh:
+            pdb = Bio.PDB.PDBParser().get_structure("lys", string_fh)
+            res = list(pdb.get_residues())[0]
+            angles = rot.Sidechain.calculate_angles(res)
+
+            # Instantiate objects and compare them
+            sc1 = rot.Sidechain("LYS", angles)
+            sc2 = rot.Sidechain.calculate(res)
+
+            # Check angles are good against known values
+            self.assertTrue(sc1.isclose(sc2), "Instantiated side-chains match")
+            self.assertEqual(len(angles), 4, "Lysine has 4 χ angles")
+            self.assertAlmostEqual(angles[0], 213.1, 1, "Parsed χ1 of Lysine")
+            self.assertAlmostEqual(angles[1], 175.0, 1, "Parsed χ2 of Lysine")
+            self.assertAlmostEqual(angles[2], 69.7, 1, "Parsed χ3 of Lysine")
+            self.assertAlmostEqual(angles[3], 201.5, 1, "Parsed χ4 of Lysine")
+
+    def test_missing_valine(self):
+        """Check an error is thrown when parsing small
+        residues (VAL) with missing atoms."""
+        with io.StringIO(self.VALINE) as string_fh:
+            pdb = Bio.PDB.PDBParser().get_structure("val", string_fh)
+            res = list(pdb.get_residues())[0]
+            del res["CB"]
+        with self.assertRaises(rot.MissingAtomError) as err:
+            rot.Sidechain.calculate_angles(res)
+            self.check_exception(err.exception, res, "CB", 0)
+
+    def test_missing_lysine(self):
+        """Check an error is thrown when parsing large residues (LYS) with
+        missing atoms."""
+        with io.StringIO(self.LYSINE) as string_fh:
+            pdb = Bio.PDB.PDBParser().get_structure("yls", string_fh)
+            res = list(pdb.get_residues())[0]
+
+        with self.assertRaises(rot.MissingAtomError) as err:
+            copy = res.copy()
+            del copy["CB"]
+            rot.Sidechain.calculate_angles(copy)
+            self.check_exception(err.exception, copy, "CB", 0)
+
+        with self.assertRaises(rot.MissingAtomError) as err:
+            copy = res.copy()
+            del copy["CD"]
+            rot.Sidechain.calculate_angles(copy)
+            self.check_exception(err.exception, copy, "CD", 1)
+
+        with self.assertRaises(rot.MissingAtomError) as err:
+            copy = res.copy()
+            del copy["CE"]
+            rot.Sidechain.calculate_angles(copy)
+            self.check_exception(err.exception, copy, "CE", 2)
+
+        with self.assertRaises(rot.MissingAtomError) as err:
+            copy = res.copy()
+            del copy["NZ"]
+            rot.Sidechain.calculate_angles(copy)
+            self.check_exception(err.exception, copy, "NZ", 3)
+
+
+    def check_exception(self, err, res, atom, chi):
+        self.assertIs(err.residue, res, "Error references residue")
+        self.assertEqual(err.atom, atom, "Error specifies missing atom")
+        self.assertEqual(err.chi, chi, "Error specifies correct chi angle")
+
+class TestRotamer(unittest.TestCase):
+    """Test rotameric state lookup."""
+    test_angles = {
+        'ASN': {'p0': [113.8016194939627, 104.31024615711995]},
+        'ASP': {'m-30': [273.8578642811458, 99.9528171316109],
+                'p0': [12.358138517160553, 58.38652535571098]},
+        'GLU': {'mm-30': [301.9582353204332, 308.24225036375265, 49.574469863477276]},
+        'HIS': {'m170': [292.52864130315095, 164.28087964338357]},
+        'ILE': {'tt': [186.7757499110814, 203.0559073373177]},
+        'LEU': {'mt': [304.7679950634703, 209.51610172332454]},
+        'MET': {'mmt': [309.79765829339635, 251.07803634775843, 146.83305424774474],
+                'pmt': [25.53476818302732, 250.44106385629553, 187.12991183038932]},
+        'MSE': {'mmp': [355.7788063563555, 357.90907007038936, 74.92002993691608]},
+        'SER': {'t': [138.10591036525867]},
+        'TRP': {'m-10': [335.30908697832575, 34.39320381502648],
+                't-100': [239.46934245004968, 213.60986822403243]},
+        'TYR': {'m-80': [259.57791656933944, 85.09418235484242]}
+    }
+
+    def test_valid_angles(self):
+        """Test that each of our test angles is valid for that rotamer."""
+        for aa, rots in self.test_angles.items():
+            for rotamer, angles in rots.items():
+                self.assertTrue(
+                    rot.Rotamer(aa, rotamer).valid_chi(angles),
+                    "Rotamer {} of AA {} is valid".format(rotamer, aa))
+        self.assertFalse(
+            rot.Rotamer("MET", "mmt").valid_chi((10, 10, 10)),
+            "Invalid rotamer")
+
+    def test_find_rotamers(self):
+        """Test that we find the correct rotamer for each angle."""
+        for aa, rots in self.test_angles.items():
+            for rotamer, angles in rots.items():
+                self.assertEqual(
+                    rotamer,
+                    rot.Rotamer.find(rot.Sidechain(aa, angles)).name,
+                    "Found rotamer {} of {}".format(rotamer, aa))
---
+++ phyre_engine/tools/rotamer/__init__.py
@@ -0,0 +1,181 @@
+"""
+This module contains tools to analyse side-chain rotamers.
+"""
+import math
+
+import Bio.PDB
+
+from . import data
+from Bio.PDB.Residue import Residue
+from Bio.codonalign.chisq import chisqprob
+
+class Sidechain:
+    """
+    Representation of a side-chain, complete with angles and rotamer.
+
+    :ivar angles: Tuple of angles (in degrees) corresponding to each χ angle.
+    :ivar res_name: Name of the residue type.
+
+    :vartype angles: tuple of floats
+    :vartype res_name: str
+    """
+
+    def __init__(self, res_name, angles):
+        """
+        Initialise a new side-chain.
+
+        :param str res_name: Residue type (three letters)
+        :param tuple(float) angles: Tuple of angles
+        :param `Bio.PDB.Residue` residue: Residue object to parse.
+        """
+        self.res_name = res_name
+        self.angles = angles
+
+    @classmethod
+    def calculate(cls, residue):
+        """
+        Calculate side-chain angles from a residue.
+
+        :param `Bio.PDB.Residue` residue: Residue to parse.
+        :raise MissingAtomError: If an atom is missing from
+        :return: A `Sidechain` object populated with the calculated angles.
+        """
+        angles = cls.calculate_angles(residue)
+        return cls(residue.get_resname(), angles)
+
+    @staticmethod
+    def calculate_angles(residue):
+        """
+        Calculate χ angles for a given residue.
+
+        :param `Bio.PDB.Residue` residue: Residue to parse.
+        :return: Tuple of angles (in degree)
+        :rtype: tuple(float)
+
+        :raise MissingAtomError: When a required atom is not found.
+        """
+        num_angles = data.NUM_CHI_ANGLES[residue.get_resname()]
+        atom_names = data.CHI_ATOMS[residue.get_resname()]
+        angles = [None] * num_angles
+
+        for i in range(0, num_angles):
+            chi_atoms = []
+            for atom in atom_names[i]:
+                if atom not in residue:
+                    raise MissingAtomError(residue, i, atom)
+                else:
+                    chi_atoms.append(residue[atom].get_vector())
+
+            dihedral = math.degrees(Bio.PDB.calc_dihedral(*chi_atoms))
+            if dihedral < 0:
+                dihedral += 360
+            angles[i] = dihedral
+        return tuple(angles)
+
+    def isclose(self, other, rel_tol=1e-09, abs_tol=0.0):
+        """
+        Check whether two rotamers are approximately the same.
+
+        This will return `True` if the side-chains share the same residue name
+        and the angles are approximately equal. Approximate equality is
+        calculated using the `math.isclose` function.
+
+        :param `Sidechain` other: Side-chain to compare.
+        :param float rel_tol: Relative tolerance (see `math.isclose`.)
+        :param float abs_tol: Absolute tolerance (see `math.isclose`.)
+        :return: Boolean indicating whether side-chains are similar.
+        .. seealso::
+        Function `math.isclose`.
+        """
+        if self.res_name != other.res_name:
+            return False
+        for a, b in zip(self.angles, other.angles):
+            if not math.isclose(a, b, rel_tol=rel_tol, abs_tol=abs_tol):
+                return False
+        return True
+
+class Rotamer:
+    """
+    Class representing a rotamer.
+
+    :ivar ranges: Tuple of ranges. A range is a two-element tuple containing the
+        start and end angles (in degrees, from 0--360).
+    :ivar name: Name of this tuple.
+    """
+
+    def __init__(self, res_name, rot_name):
+        """
+        Instantiate a new `Rotamer` object.
+
+        :param str res_name: Name of the amino acid type of this rotamer.
+        :param str rot_name: Rotamer name.
+        """
+        self.ranges = data.ROTAMERS[res_name][rot_name]
+        self.name = rot_name
+
+    @classmethod
+    def find(cls, sidechain):
+        """
+        Returns the rotameric state of the given sidechain.
+
+        :param `Sidechain` sidechain: The side-chain to search for.
+        :return: Rotameric state of the sidechain or `None` for outliers.
+        """
+
+        # We'll just do a linear search for the moment, because I don't expect
+        # this to be part of any high-performance pipelines. It would probably
+        # be possible to reduce this to O(1) because we know the start and stop
+        # of each range tend to be the same values.
+        for rot in ALL_ROTAMERS[sidechain.res_name].values():
+            if rot.valid_chi(sidechain.angles):
+                return rot
+        return None
+
+    def valid_chi(self, angles):
+        """
+        Check whether each χ angle falls within the allowed range.
+
+        :param angles: Tuple of angles to check.
+        :return: `True` if all angles are valid or `False` otherwise.
+        """
+        for i, chi in enumerate(angles):
+            if not (self.ranges[i][0] <= chi < self.ranges[i][1]):
+                return False
+        return True
+
+class MissingAtomError(ValueError):
+    """Thrown when a χ angle cannot be calculated due to missing angles.
+
+    :ivar residue: The residue that is missing an atom.
+    :ivar chi: The index of the χ angle (starting from 0).
+    :ivar atom: The name of the missing atom.
+
+    :vartype residue: `Bio.PDB.Residue`
+    :vartype chi: int
+    :vartype atom: str
+    """
+
+    _ERR_MSG = "Atom {atom} missing when calculating χ angle {chi} of " \
+        "residue {residue}"
+
+    def __init__(self, residue, chi, atom):
+        """
+        Initialise a new exception.
+
+        :param `Bio.PDB.Residue` residue: The residue that is missing atoms.
+        :param int chi: Index of the χ angle (starting from 0).
+        :param str atom: Name of the missing atom.
+        """
+        super().__init__(
+            self._ERR_MSG.format(atom=atom, residue=residue, chi=chi))
+        self.residue = residue
+        self.chi = chi
+        self.atom = atom
+
+
+# Pre-load list of rotamers
+ALL_ROTAMERS = {}
+for aa, rots in data.ROTAMERS.items():
+    ALL_ROTAMERS[aa] = {}
+    for rot_name in rots.keys():
+        ALL_ROTAMERS[aa][rot_name] = Rotamer(aa, rot_name)
