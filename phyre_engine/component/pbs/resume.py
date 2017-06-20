0a7fae56b4f82a5e0468fea0fe5d2d398aa9d3a6
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-06-20 16:03:29 +0100

    Initialise logger before trying to load results

--- phyre_engine/component/pbs/resume.py
+++ phyre_engine/component/pbs/resume.py
@@ -15,6 +15,8 @@ from pathlib import Path
 from phyre_engine.component.pbs import qsub
 import logging.config
 
+log = lambda: logging.getLogger(__name__)
+
 def arg_parser():
     # Setup argument parser
     parser = ArgumentParser(description=__import__('__main__').__doc__)
@@ -38,29 +40,38 @@ def main():  # IGNORE:C0111
         args = parser.parse_args()
         storage_dir = Path(args.storage_dir)
 
-        # Generate the new state
-        state = None
-        for i in range(0, args.num_jobs):
-            state_file = storage_dir / qsub.WORKER_STATE.format(i)
-            with state_file.open("rb") as state_in:
-                sub_state = pickle.load(state_in)
-                if not isinstance(sub_state, qsub.CompletedState):
-                    raise ValueError("No data in {}".format(state_file))
-
-                if state is None:
-                    state = sub_state.data
-                else:
-                    state[args.join_var].extend(sub_state.data[args.join_var])
-
         # Get the pipeline to resume
         with (storage_dir / qsub.RESUME_PIPELINE).open("rb") as pipe_in:
             pipe_state = pickle.load(pipe_in)
-            pipe_state.pipeline.start = state
             # Initialise loggers
             if "logging" in pipe_state.config:
                 logging.config.dictConfig(pipe_state.config["logging"])
 
-            pipe_state.pipeline.run(pipe_state.pipeline_index + 1)
+
+        # Generate the new state
+        state = None
+        for i in range(0, args.num_jobs):
+            state_file = storage_dir / qsub.WORKER_STATE.format(i)
+            try:
+                with state_file.open("rb") as state_in:
+                    sub_state = pickle.load(state_in)
+                    if not isinstance(sub_state, qsub.CompletedState):
+                        raise ValueError("No data in {}".format(state_file))
+
+                    if state is None:
+                        state = sub_state.data
+                    else:
+                        sub_slice = sub_state.data[args.join_var]
+                        state[args.join_var].extend(sub_slice)
+            except Exception as err:
+                log().error(
+                    "Error reading data from worker %d (%s)",
+                    i, str(state_file),
+                    exc_info=err)
+                raise err
+
+        pipe_state.pipeline.start = state
+        pipe_state.pipeline.run(pipe_state.pipeline_index + 1)
 
         return 0
     except KeyboardInterrupt:
