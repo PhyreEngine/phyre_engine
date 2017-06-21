15a9a15d0f3e15cac767f79b26ae3744a7f9ce3f
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-06-21 15:18:32 +0100

    NameTemplate more easily called from scripts
    Previously, the .db.NameTemplate required a lambda function to determine
    how to split the sequence name, which couldn't safely be passed in from
    user input. This commit adds some built-in functions that can be
    referred to by name.

--- phyre_engine/test/component/test_db.py
+++ phyre_engine/test/component/test_db.py
@@ -6,6 +6,9 @@ import phyre_engine.component.db as db
 import phyre_engine.conformation as conformation
 from Bio.PDB import PDBParser
 from pathlib import Path
+import io
+import Bio.SeqIO
+from phyre_engine.component.db import NameTemplate
 
 class TestRCSBClusterDownload(unittest.TestCase):
     """Test downloading cluster file from the RCSB"""
@@ -239,6 +242,41 @@ class TestSimpleDBPipeline(unittest.TestCase):
                     Path("{}.ffdata".format(db_prefix)).exists(),
                     "ffdata file exists")
 
+class TestNameTemplate(unittest.TestCase):
+    """Test that NameTemplate can assign names correctly."""
+
+    FASTA = ">FOO.BAR-BAZ-QUX\nAAAEEEAAA"
+
+    def setUp(self):
+        with io.StringIO(self.FASTA) as seq_fh:
+            self.data = {
+                "templates": [{"sequence": Bio.SeqIO.read(seq_fh, "fasta")}]}
+
+    @staticmethod
+    def result_name(data):
+        return data["templates"][0]["name"]
+
+    def test_builtin_full(self):
+        """Using full_name builtin"""
+        self.assertEqual(
+            self.result_name(NameTemplate("full_name").run(self.data)),
+            "FOO.BAR-BAZ-QUX")
+
+    def test_builtin_split(self):
+        """Using split builtin"""
+        self.assertEqual(
+            self.result_name(NameTemplate("split", ".", 0).run(self.data)),
+            "FOO")
+        self.assertEqual(
+            self.result_name(NameTemplate("split", ".", 1).run(self.data)),
+            "BAR-BAZ-QUX")
+
+    def test_custom(self):
+        """Using custom function"""
+        custom_fn = lambda t: "custom"
+        self.assertEqual(
+            self.result_name(NameTemplate(custom_fn).run(self.data)),
+            "custom")
 
 if __name__ == "__main__":
     unittest.main()
