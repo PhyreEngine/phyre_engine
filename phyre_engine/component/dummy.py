fadc7c8b4aec3225afd525054829b3b28974e557
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-06-30 19:36:27 +0100

    Add more dummy components for testing

--- phyre_engine/component/dummy.py
+++ phyre_engine/component/dummy.py
@@ -1,6 +1,8 @@
 from phyre_engine.component.component import Component
-import pprint
 import logging
+import time
+import pickle
+import os
 
 log = lambda: logging.getLogger(__name__)
 
@@ -9,11 +11,71 @@ class Dummy(Component):
     REMOVES = []
     REQUIRED = []
 
-    def __init__(self, prefix):
+    def __init__(self, prefix, add=None):
         self.prefix = prefix
+        self.add = add if add is not None else []
 
     def run(self, data, config=None, pipeline=None):
-        log().warning("Dummy warning")
-        for ln in pprint.pformat(data).split("\n"):
-            print("{}: {}".format(self.prefix, ln))
+        log().warning("Dummy warning: %s", self.prefix)
+        data[self.prefix] = self.add
+        return data
+
+class Multiply(Component):
+    ADDS = []
+    REMOVES = []
+    REQUIRED = []
+
+    def __init__(self, var, by):
+        self.var = var
+        self.by = by
+
+    def run(self, data, config=None, pipeline=None):
+        data[self.var] = [x * self.by for x in data[self.var]]
+        return data
+
+class Sleep(Component):
+    ADDS = []
+    REMOVES = []
+    REQUIRED = []
+
+    def __init__(self, seconds):
+        self.seconds = seconds
+
+    def run(self, data, config=None, pipeline=None):
+        time.sleep(self.seconds)
+        return data
+
+class Dump(Component):
+    ADDS = []
+    REMOVES = []
+    REQUIRED = []
+
+    def __init__(self, file):
+        self.file = file
+
+    def run(self, data, config=None, pipeline=None):
+        with open(self.file, "wb") as file_out:
+            pickle.dump(data, file_out)
+        return data
+
+class Hostname(Component):
+    ADDS = ["hostname"]
+    REMOVES = []
+    REQUIRED = []
+
+    def run(self, data, config=None, pipeline=None):
+        data["hostname"] = os.uname().nodename
+        return data
+
+class LogTest(Component):
+    ADDS = []
+    REMOVES = []
+    REQUIRED = []
+
+    def run(self, data, config=None, pipeline=None):
+        log().debug("Debug message")
+        log().info("Info message")
+        log().warn("Warning message")
+        log().error("Error message")
+        log().critical("Critical error message")
         return data
