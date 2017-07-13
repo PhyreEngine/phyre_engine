00de4becda7fc0ed13a6e66bcdad6c44420580e1
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-07-13 19:27:33 +0100

    Add decorators for skipping tests
    Also add tests for those decorators.

--- phyre_engine/test/__init__.py
+++ phyre_engine/test/__init__.py
@@ -1,2 +1,82 @@
-#: Can be set by the test runner.
+"""
+Module containing utilities to make testing slightly easier.
+
+PhyreEngine is designed to implement bioinformatics pipelines, and so it
+contains several components that wrap external tools. These components require
+some configuration, which cannot be taken for granted, and so some of the
+PhyreEngine tests rely on the variable :py:data:`phyre_engine.test.config`,
+which may contain a test configuration.
+
+As well as defining :py:data:`phyre_engine.test.config`, this module contains
+decorators aiming to make it easier to skip tests if the required configuration
+parameters are not set.
+"""
+
+# The global variable "config" is not a constant, and so shouldn't be uppercase.
+# Similarly, the decorators defined here are in camelCase because that is the
+# (annoying) convention used by the unittest module.
+# pylint: disable=invalid-name
+
+import unittest
+import functools
+
+#: Can be set by the test runner
 config = None
+
+def requireConfig(func):
+    """Decorator used to skip a test if the test configuration is not set."""
+    if config is None:
+        return unittest.skip("Test requires configuration to be set")(func)
+    return func
+
+def requireFields(fields, parents=None):
+    """
+    Decorator used to skip a test if the given fields are not set in the test
+    configuration.
+
+    For example, the following usage requires the fields ``foo`` and ``bar`` in
+    the section ``section2``, which must be nested under ``section1``:
+
+    >>> from phyre_engine.test import requireFields
+    >>> @requireFields(["foo", "bar"], ["section1", "section2"])
+    ... def test_widget():
+    ...     ...
+
+    This would correspond to the following configuration file (in YAML format):
+
+    .. code-block:: YAML
+
+        section1:
+            section2:
+                foo: true
+                bar: "Hello world"
+
+    :param list[str] fields: List of fields that must be defined.
+    :param list[str] parents: Section of the configuration in which fields must
+        be present.
+    """
+    if config is None:
+        return unittest.skip("Test requires configuration to be set")
+
+    parents = parents if parents is not None else []
+
+    # pylint: disable=unsubscriptable-object, unsupported-membership-test
+    config_section = config
+    for i, parent in enumerate(parents):
+        if parent not in config_section:
+            return unittest.skip(
+                "Section {sec} not found in {previous}".format(
+                    sec=parent,
+                    previous=parents[0:i]))
+        config_section = config_section[parent]
+    for field in fields:
+        if field not in config_section:
+            return unittest.skip("Field {field} not found".format(field=field))
+
+    # pylint: disable=missing-docstring
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
---
+++ phyre_engine/test/test_test.py
@@ -0,0 +1,229 @@
+"""This module tests our custom test decorators."""
+import unittest
+import phyre_engine.test
+
+# Missing docstrings are for our dummy functions, and invalid-name warnings are
+# for methods overriden from TestCase.
+# pylint: disable=missing-docstring, invalid-name
+
+#: We will save the original test config here and restore it later.
+original_config = None
+
+# Sample data used for testing
+_FIELDS = ["foo", "bar"]
+_SECTIONS = ["section1", "section2"]
+_SAMPLE_CONFIG = {
+    "section1": {
+        "section2": {
+            "foo": True,
+            "bar": "baz"
+        }
+    }
+}
+
+def setUpModule():
+    """
+    This test suite edits the :py:data:`phyre_engine.test.config` variable so
+    that we can test our custom test function, but is probably going to be run
+    as part of the standard unit test suite. To hopefully prevent this module
+    from stomping the test configuration, we store it here and reset it later.
+
+    .. warning::
+
+        This is probably going to break absolutely everything if we switch to
+        thread-based parallel testing.
+    """
+    global original_config  # pylint: disable=global-statement
+    original_config = phyre_engine.test.config
+
+def tearDownModule():
+    """Reset :py:data:`phyre_engine.test.config` variable."""
+    phyre_engine.test.config = original_config
+
+class TestRequireConfig(unittest.TestCase):
+    """
+    The :py:func:`phyre_engine.test.requireConfig` decorator should skip tests
+    if no config file was supplied.
+    """
+
+    def setUp(self):
+        """
+        Set the phyre_engine.test.config variable to None before each test.
+        This is so that we can be sure of its value when we are testing it.
+        """
+        phyre_engine.test.config = None
+
+    def test_missing(self):
+        """@requireConfig should cause tests to skip if no config is set."""
+
+        # config is not set, so calling this should raise a SkipTest exception.
+        @phyre_engine.test.requireConfig
+        def dummy_fail():
+            return 1
+        self.assertRaises(unittest.SkipTest, dummy_fail, "Test skipped")
+
+    def test_present(self):
+        """@requireConfig should not skip if config is set."""
+        try:
+            phyre_engine.test.config = True
+            @phyre_engine.test.requireConfig
+            def dummy_pass():
+                return 1
+            self.assertEqual(dummy_pass(), 1, "Test not skipped")
+        except unittest.SkipTest:
+            self.fail("Test was skipped erroneously")
+
+class TestRequireFields(unittest.TestCase):
+    """
+    The :py:func:`phyre_engine.test.requireFields` decorator should skip tests
+    if the configuration is missing or the required fields are missing.
+    """
+
+    def setUp(self):
+        """Clean config before tests and set some values to look up."""
+        phyre_engine.test.config = None
+
+    def test_missing_config(self):
+        """@requireFields should skip if config is not set."""
+
+        # This should fail because the configuration is None
+        @phyre_engine.test.requireFields(_FIELDS, _SECTIONS)
+        def dummy_fail_none():
+            return 1
+        self.assertRaises(
+            unittest.SkipTest, dummy_fail_none,
+            "Test skipped with config == None")
+
+    def test_good_config(self):
+        """Set test config to something useful and require those fields."""
+        phyre_engine.test.config = _SAMPLE_CONFIG
+
+        try:
+            @phyre_engine.test.requireFields(_FIELDS, _SECTIONS)
+            def dummy_pass():
+                return 1
+            self.assertEqual(
+                dummy_pass(), 1,
+                "Function call succeeds with correct config.")
+        except unittest.SkipTest:
+            self.fail("Test was skipped erroneously")
+
+    def test_missing_field(self):
+        """Fields not present in the config should cause a skip."""
+
+        @phyre_engine.test.requireFields(_FIELDS + ["qux"], _SECTIONS)
+        def dummy_fail_missing():
+            return 1
+        self.assertRaises(
+            unittest.SkipTest, dummy_fail_missing,
+            "Test skipped when missing fields")
+
+    def test_missing_section(self):
+        """Sections not present in the config should cause a skip."""
+
+        @phyre_engine.test.requireFields(_FIELDS, _SECTIONS + ["qux"])
+        def dummy_fail_missing():
+            return 1
+        self.assertRaises(
+            unittest.SkipTest, dummy_fail_missing,
+            "Test skipped when missing sections")
+
+class TestWithMissingConfig(unittest.TestCase):
+    """
+    This test case uses our test decorators in the wild, wrapped around test
+    methods. This class sets the config to be ``None`` and ensure that
+    decorators skip tests that rely on the config.
+    """
+
+    # Yeah, setting a module variable in class scope is awful. I'm aware.
+    phyre_engine.test.config = None
+
+    @phyre_engine.test.requireConfig
+    def test_require_config(self):
+        """This should be skipped because config is None."""
+        self.fail("This should have been skipped.")
+
+    @phyre_engine.test.requireFields(["foo"])
+    def test_require_fields(self):
+        """This should be skipped because config is None."""
+        self.fail("This should have been skipped.")
+
+class TestWithConfig(unittest.TestCase):
+    """
+    Set a test configuration and make sure that our custom test decorators work
+    with it when attached to actual test cases.
+    """
+
+    # Still awful
+    phyre_engine.test.config = _SAMPLE_CONFIG
+
+    @phyre_engine.test.requireConfig
+    def test_require_config(self):
+        """This should be run because test config was not None."""
+        pass
+
+    @phyre_engine.test.requireFields(_FIELDS, _SECTIONS)
+    def test_require_fields_valid(self):
+        """This should be run because test config is valid."""
+        pass
+
+    @phyre_engine.test.requireFields(_FIELDS + ["qux"], _SECTIONS)
+    def test_require_fields_invalid(self):
+        """This should not run because field "qux" is not in config."""
+        self.fail("This should have been skipped.")
+
+    @phyre_engine.test.requireFields(_FIELDS, _SECTIONS + ["qux"])
+    def test_require_sections_invalid(self):
+        """This should not run because section "qux" is not in config."""
+        self.fail("This should have been skipped.")
+
+phyre_engine.test.config = None
+@phyre_engine.test.requireConfig
+class TestRequireConfigClassSkipped(unittest.TestCase):
+    """@requireConfig should skip this class because config is None."""
+
+    def test_skipped(self):
+        """We should have been skipped."""
+        self.fail()
+
+@phyre_engine.test.requireFields(_FIELDS, _SECTIONS)
+class TestRequireFieldsClassSkipped(unittest.TestCase):
+    """@requireFields should skip this class because config is None."""
+
+    def test_skipped(self):
+        """We should have been skipped."""
+        self.fail()
+
+phyre_engine.test.config = {}
+@phyre_engine.test.requireConfig
+class TestRequireConfigClassNotSkipped(unittest.TestCase):
+    """This class should NOT be skipped because the config is not None."""
+
+    def test_not_skipped(self):
+        """Class not skipped."""
+        pass
+
+phyre_engine.test.config = _SAMPLE_CONFIG
+@phyre_engine.test.requireFields(_FIELDS, _SECTIONS)
+class TestRequireFieldsClassNotSkipped(unittest.TestCase):
+    """@requireFields should NOT skip this class."""
+
+    def test_skipped(self):
+        """Class not skipped."""
+        pass
+
+@phyre_engine.test.requireFields(_FIELDS + ["qux"], _SECTIONS)
+class TestRequireFieldsClassSkippedWithInvalidFields(unittest.TestCase):
+    """@requireFields should skip this class because of invalid fields."""
+
+    def test_skipped(self):
+        """We should have been skipped."""
+        self.fail()
+
+@phyre_engine.test.requireFields(_FIELDS, _SECTIONS + ["qux"])
+class TestRequireFieldsClassSkippedWithInvalidSections(unittest.TestCase):
+    """@requireFields should skip this class because of invalid sections."""
+
+    def test_skipped(self):
+        """We should have been skipped."""
+        self.fail()
