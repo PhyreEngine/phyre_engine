3c02858c8e64ef0c8b5a3d435d124778b638c400
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-07-17 15:02:43 +0100

    Move PDB-related functions to a tools module

--- phyre_engine/component/db/db.py
+++ phyre_engine/component/db/db.py
@@ -1,16 +1,9 @@
 from phyre_engine.component import Component
+import phyre_engine.tools.pdb as pdb
 from enum import Enum
-import gzip
 import pathlib
-import json
 import urllib.request
-import Bio.Seq
-import Bio.SeqIO
 import Bio.PDB
-import Bio.Alphabet.IUPAC
-from Bio.PDB.Chain import Chain
-from Bio.PDB.Residue import Residue
-import contextlib
 import logging
 
 log = lambda: logging.getLogger(__name__)
@@ -18,9 +11,6 @@ log = lambda: logging.getLogger(__name__)
 class StructureType(Enum):
     PDB = "pdb"
     MMCIF = "cif"
-
-# Used when searching for a PDB file of arbitrary type
-_STRUCTURE_SUFFIXES = (".pdb", ".pdb.gz", ".cif", ".cif.gz")
 
 class StructureRetriever(Component):
     """
@@ -58,7 +48,7 @@ class StructureRetriever(Component):
                 PDB=template["PDB"].lower(),
                 type=self.struc_type.value)
 
-            path = pdb_path(
+            path = pdb.pdb_path(
                 template["PDB"],
                 ".{}.gz".format(self.struc_type.value),
                 base_dir=self.base_dir)
@@ -115,14 +105,14 @@ class ChainPDBBuilder(Component):
             pdb_id = template["PDB"]
             chain = template["chain"]
 
-            source_file = find_pdb(pdb_id, base_dir=self.mmcif_dir)
+            source_file = pdb.find_pdb(pdb_id, base_dir=self.mmcif_dir)
             if source_file is None:
                 log().error(
                     "Could not find MMCIF file '%s' in '%s'",
                     pdb_id, self.mmcif_dir)
                 continue
 
-            pdb_file = pdb_path(pdb_id, ".pdb", chain, self.pdb_dir)
+            pdb_file = pdb.pdb_path(pdb_id, ".pdb", chain, self.pdb_dir)
             pdb_file.parent.mkdir(parents=True, exist_ok=True)
 
             if not pdb_file.exists() or self.overwrite:
@@ -134,13 +124,10 @@ class ChainPDBBuilder(Component):
 
                 for selector in self.conf_sel:
                     chain = selector.select(chain)
-                mapping, chain = self._renumber(chain, ".")
+                mapping, chain = pdb.renumber(chain, ".")
 
                 with pdb_file.open("w") as pdb_out:
-                    json_str = json.dumps(mapping)
-                    for map_line in _chunk_string(json_str, 69):
-                        print("REMARK 149 " + map_line, file=pdb_out)
-
+                    pdb.write_json_remark(pdb_out, mapping, 149)
                     pdbio.set_structure(chain)
                     pdbio.save(pdb_out)
             template["structure"] = str(pdb_file)
@@ -148,34 +135,6 @@ class ChainPDBBuilder(Component):
 
         data["templates"] = new_templates
         return data
-
-    def _renumber(self, chain, new_id=" "):
-        """
-        Renumber a chain from 1, stripping insertion codes.
-
-        :param `Bio.PDB.Chain` chain: structure to sanitise.
-        :param str new_id: ID of the new chain.
-        :return: A 2-tuple containing the following:
-
-            1. The new :py:class:`Bio.PDB.Chain.Chain` object.
-            2. A list of 2-tuples containing the new residue index and old
-               residue ID. The new residue index is an integer and the old is
-               the 3-tuple returned by :py:meth:`Bio.PDB.Chain.Chain.get_id`.
-        """
-        mapping = []
-        sanitised_chain = Chain(new_id)
-
-        for res_index, res in enumerate(chain):
-            sanitised_res = Residue(
-                (' ', res_index + 1, ' '),
-                res.get_resname(),
-                res.get_segid())
-            mapping.append((res_index + 1, res.get_id()))
-
-            for atom in res:
-                sanitised_res.add(atom.copy())
-            sanitised_chain.add(sanitised_res)
-        return mapping, sanitised_chain
 
 class PDBSequence(Component):
     """
@@ -200,105 +159,6 @@ class PDBSequence(Component):
         for template in templates:
             structure = parser.get_structure("", template["structure"])
             chain = list(structure[0].get_chains())[0]
-            template["sequence"], _ = atom_seq(chain)
+            template["sequence"], _ = pdb.atom_seq(chain)
         return data
 
-@contextlib.contextmanager
-def open_pdb(path):
-    """
-    Open a PDB or MMCIF file. This should be used as a context manager, and
-    automatically handles decompressing gzipped files.
-    """
-    try:
-        if path.suffix == ".gz":
-            stream = gzip.open(str(path), "rt")
-            yield stream
-        else:
-            stream = path.open("r")
-            yield stream
-    finally:
-        stream.close()
-
-def find_pdb(pdb_name, *args, suffix_list=_STRUCTURE_SUFFIXES, **kwargs):
-    """
-    For each suffix, try and find a matching file using :py:func:`.pdb_path`.
-
-    :return: Path to the file or ``None`` if no matching file could be found.
-    :rtype: :py:class:`pathlib.Path` object.
-    """
-
-    for suffix in suffix_list:
-        path = pdb_path(pdb_name, suffix, *args, **kwargs)
-        if path.exists():
-            return path
-    return None
-
-def pdb_path(pdb_name, suffix, chain_id=None, base_dir=None):
-    """
-    Gives the directory in which to save a PDB or MMCIF file.
-
-    If the ``base_dir`` parameter is supplied, paths are given relative to the
-    base directory. If the ``chain_id`` is not supplied, then the path of the
-    PDB file ``1xyz.pdb`` will be ``xy/1xyz.pdb``. If the ``chain_id`` is
-    supplied, then the PDB ID is a separate trailing subdirectory and the chain
-    ID is appended before any file extensions.
-
-    :param str pdb_name: Four-letter PDB identifier.
-    :param str suffix: Suffix to use (``.pdb`` is probably useful).
-    :param str chain_id: Optional chain ID. If supplied, it is assumed that
-        chains are stored in separate files, in a subdirectory named after the
-        PDB ID.
-    :param str base_dir: Optional base directory.
-
-    :return: The path of the PDB file.
-    :rtype: Object from :py:mod:`pathlib`.
-
-    >>> from phyre_engine.component.db.db import pdb_path
-    >>> pdb_path("1ABC", ".pdb")
-    PosixPath("ab/1abc.pdb")
-    >>> pdb_path("1ABC", ".cif.gz", "A")
-    PosixPath("ab/1abc/1abc_A.cif.gz")
-    >>> pdb_path("1ABC", ".cif.gz", "A", "/cif/dir")
-    PosixPath("/cif/dir/ab/1abc/1abc_A.cif.gz")
-    """
-
-    middle = pdb_name[1:3].lower()
-    pdb_id = pdb_name.lower()
-
-    root = pathlib.Path(base_dir) if base_dir is not None else pathlib.Path(".")
-    root = root / middle
-    if chain_id is not None:
-        root = root / pdb_id / "{}_{}{}".format(pdb_id, chain_id, suffix)
-    else:
-        root = root / "{}{}".format(pdb_id, suffix)
-    return root
-
-def _chunk_string(string, size):
-    """Split string into equal length chunks. Used for dumping JSON data into
-    the REMARK section."""
-    return (string[i:i + size] for i in range(0, len(string), size))
-
-def atom_seq(chain):
-    """
-    Return the amino acid sequence of ``chain``, along with a list of
-    :py:class:`Bio.PDB.Residue.Residue` objects corresponding to the chosen
-    sequence.
-
-    This function does the following filtering:
-
-    1. Discards all HETATMs.
-    2. Discards everything but the standard 20 amino acids.
-    3. Discard all residues without a CA atom defined.
-
-    This is intended to be the canonical source of a PDB structure's sequence.
-    """
-
-    sequence = ""
-    residues = []
-    for res in chain:
-        if (res.get_id()[0] == ' '
-                and Bio.PDB.Polypeptide.is_aa(res, True)
-                and "CA" in res):
-            sequence += Bio.PDB.Polypeptide.three_to_one(res.get_resname())
-            residues.append(res)
-    return Bio.Seq.Seq(sequence, Bio.Alphabet.IUPAC.IUPACProtein), residues
--- phyre_engine/test/component/db/test_db.py
+++ phyre_engine/test/component/db/test_db.py
@@ -3,18 +3,10 @@ import tempfile
 import unittest
 import phyre_engine.component.db.db as db
 import phyre_engine.test.data
+import phyre_engine.test.tools.test_pdb
 from pathlib import Path
 import shutil
 import Bio.SeqIO
-import Bio.PDB
-import io
-
-_ATOM_SEQ_TEST_PDB = (
-    "HETATM    1  CA  FOO A   1      11.751  37.846  29.016  1.00 44.65\n"
-    "ATOM      2  CA  ALA A   2      12.501  39.048  28.539  1.00 30.68\n"
-    "ATOM      3  C   ALA A   2      13.740  38.628  27.754  1.00 24.74\n"
-    "ATOM      4  CB  GLU A   3      14.207  37.495  27.890  1.00 25.59\n"
-    "ATOM      5  CA  GLY A   4      14.207  37.495  27.890  1.00 25.59\n")
 
 class TestStructureRetriever(unittest.TestCase):
     """Test STructureRetriever component."""
@@ -46,61 +38,6 @@ class TestStructureRetriever(unittest.TestCase):
                     self.assertTrue(
                         pdb_4hhb.exists(),
                         "{!s} should exist".format(pdb_4hhb))
-
-class TestFunctions(unittest.TestCase):
-    """Test utility functions."""
-
-    def setUp(self):
-        self.mmcif_dir = Path(phyre_engine.test.data.__file__).parent / "mmcif"
-
-    def test_pdb_path(self):
-        """Test that our PDB path function works."""
-        self.assertEqual(
-            db.pdb_path("1ABC", ".pdb.gz"),
-            Path("ab/1abc.pdb.gz"))
-
-        self.assertEqual(
-            db.pdb_path("1AbC", ".cif"),
-            Path("ab/1abc.cif"))
-
-        self.assertEqual(
-            db.pdb_path("1abC", ".cif.gz", "A"),
-            Path("ab/1abc/1abc_A.cif.gz"))
-
-    def test_find_pdb(self):
-        """Test "find_pdb" function to find a structure."""
-        self.assertEqual(
-            db.find_pdb("12as", base_dir=self.mmcif_dir),
-            self.mmcif_dir / "2a/12as.cif")
-
-        self.assertEqual(
-            db.find_pdb("4n6v", base_dir=self.mmcif_dir),
-            self.mmcif_dir / "n6/4n6v.cif.gz")
-
-        self.assertIsNone(db.find_pdb("1abc"))
-
-    def test_open_pdb_uncompressed(self):
-        """Open an uncompressed mmcif file."""
-        pdb_path = db.pdb_path("12as", ".cif", base_dir=self.mmcif_dir)
-        with db.open_pdb(pdb_path) as pdb_in:
-            self.assertGreater(len(pdb_in.readlines()), 0)
-
-    def test_open_pdb_compressed(self):
-        """Open a compressed mmcif file."""
-        pdb_path = db.pdb_path("4n6v", ".cif.gz", base_dir=self.mmcif_dir)
-        with db.open_pdb(pdb_path) as pdb_in:
-            self.assertGreater(len(pdb_in.readlines()), 0)
-
-    def test_atom_seq(self):
-        """Get atom sequence from a PDB file."""
-        with io.StringIO(_ATOM_SEQ_TEST_PDB) as pdb_in:
-            structure = Bio.PDB.PDBParser().get_structure("test", pdb_in)
-            seq, res = db.atom_seq(structure[0]["A"])
-            self.assertEqual(
-                str(seq), "AG",
-                "Filter HETATMs and residues without a CA atom")
-            self.assertEqual(res[0].get_id(), (' ', 2, ' '))
-            self.assertEqual(res[1].get_id(), (' ', 4, ' '))
 
 
 class TestChainPDBBuilder(unittest.TestCase):
@@ -186,7 +123,7 @@ class TestPDBSequence(unittest.TestCase):
         """Sequence should match atom_seq function."""
 
         with tempfile.NamedTemporaryFile("w") as pdb_fh:
-            pdb_fh.write(_ATOM_SEQ_TEST_PDB)
+            pdb_fh.write(phyre_engine.test.tools.test_pdb.ATOM_SEQ_TEST_PDB)
             pdb_fh.flush()
             seq_parser = db.PDBSequence()
             results = seq_parser.run({
