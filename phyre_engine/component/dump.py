36351fbf5206b6903303a5999425ba759eb64f76
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-07-20 21:34:32 +0100

    Add custom encoders for SeqRecord and range
    This commit adds encoders for Bio.SeqRecord.SeqRecord objects and range
    objects to the Json and Yaml dumpers.

--- phyre_engine/component/dump.py
+++ phyre_engine/component/dump.py
@@ -4,9 +4,11 @@ formats. By default, each dumper will write to standard output. The ``output``
 parameter may be set to either a file name or output stream to determine where
 the output will be written.
 """
+import json.encoder
 import sys
 from phyre_engine.component.component import Component
 from phyre_engine.tools.util import Stream
+import Bio.SeqRecord
 
 try:
     # Use libyaml if it is available
@@ -14,6 +16,71 @@ try:
     from yaml import CSafeDumper as SafeDumper
 except ImportError:
     from yaml import SafeDumper
+
+class JsonStateEncoder(json.JSONEncoder):
+    """
+    Custom JSON encoder for pipeline state.
+
+    This decoder includes JSON serialisers for the following objects types:
+
+    :py:class:`Bio.SeqRecord` objects:
+        Sequences objects are serialised as a FASTA string.
+
+    :py:class:`range` objects:
+        Ranges are serialised as tuples containing a start and stop. Ranges with
+        a step other than 1 will result on a :py:exc:`TypeError` being raised.
+    """
+
+    def default(self, o):
+        """Called to encode a."""
+        # Disable warnings about hidden method; this is the recommended usage.
+        # pylint: disable=method-hidden
+        if isinstance(o, Bio.SeqRecord.SeqRecord):
+            return serialise_seqrecord(o)
+        elif isinstance(o, range) and o.step == 1:
+            return serialise_range(o)
+        else:
+            super().default(o)
+
+class YamlStateDumper(SafeDumper):
+    """
+    Custom decoder for pipeline state. See :py:class:`.JsonStateEncoder` for the
+    types encoded.
+    """
+    # Disable warning about too many ancestors. Don't blame me, blame pyyaml.
+    # pylint: disable=too-many-ancestors
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_representer(Bio.SeqRecord.SeqRecord, self._serialise_seqrecord)
+        self.add_representer(range, self._serialise_range)
+
+    @staticmethod
+    def _serialise_seqrecord(dumper, record):
+        return dumper.represent_scalar(
+            "tag:yaml.org,2002:str",
+            serialise_seqrecord(record),
+            style="|")
+
+    @staticmethod
+    def _serialise_range(dumper, range_obj):
+        if range_obj.step != 1:
+            raise yaml.representer.RepresenterError(
+                "Cannot represent range {!s} with step {}".format(
+                    range_obj, range_obj.step))
+
+        return dumper.represent_sequence(
+            "tag:yaml.org,2002:seq",
+            serialise_range(range_obj))
+
+
+def serialise_seqrecord(record):
+    """Use :py:mod:`Bio.SeqIO` to generate a FASTA sequence."""
+    return record.format("fasta")
+
+def serialise_range(range_obj):
+    """Return a tuple of ``(stop, start)`` values."""
+    return (range_obj.start, range_obj.stop)
 
 class Dumper(Component):
     """
@@ -36,11 +103,11 @@ class Json(Dumper):
     def run(self, data, config=None, pipeline=None):
         """Dump pipeline state."""
         with Stream(self.output, "w") as out_fh:
-            json.dump(data, out_fh)
+            json.dump(data, out_fh, cls=JsonStateEncoder)
         return data
 
 class Yaml(Dumper):
     """Dump pipeline state in YAML."""
     def run(self, data, config=None, pipeline=None):
         with Stream(self.output, "w") as out_fh:
-            yaml.dump(data, out_fh, SafeDumper)
+            yaml.dump(data, out_fh, Dumper=YamlStateDumper)
--- phyre_engine/test/component/test_dump.py
+++ phyre_engine/test/component/test_dump.py
@@ -5,11 +5,29 @@ import unittest
 import json
 import yaml
 import phyre_engine.component.dump as dump
+import Bio.SeqRecord
+import Bio.Seq
 
-class TestDumpers(unittest.TestCase):
-    """Test all dumpers."""
+# This will be serialised...
+_PIPELINE_INPUT = {
+    "foo": "bar",
+    "baz": [1, 2, 3],
+    "seq": Bio.SeqRecord.SeqRecord(
+        Bio.Seq.Seq("AHHEG"),
+        id="foo", description="desc"),
+    "range": range(0, 100)
+}
 
-    _TEST_PIPELINE = {"foo": "bar", "baz": [1, 2, 3]}
+# Into this:
+_PIPELINE_OUTPUT = {
+    "foo": "bar",
+    "baz": [1, 2, 3],
+    "seq": ">foo desc\nAHHEG\n",
+    "range": [0, 100]
+}
+
+class DumperTestBase(unittest.TestCase):
+    """Base class for testing dumpers."""
 
     def setUp(self):
         """Create an in-memory stream we can read and write from."""
@@ -19,22 +37,37 @@ class TestDumpers(unittest.TestCase):
         """Close in-memory stream."""
         self.stream.close()
 
-
     def _verify_dump(self, parsed):
         return self.assertDictEqual(
-            parsed, self._TEST_PIPELINE,
+            parsed, _PIPELINE_OUTPUT,
             "Parsed dict and written dict equal")
+
+class TestJsonDumper(DumperTestBase):
+    """Test JSON dumper."""
 
     def test_json(self):
         """Dump data in JSON format."""
         json_dumper = dump.Json(self.stream)
-        json_dumper.run(self._TEST_PIPELINE)
+        json_dumper.run(_PIPELINE_INPUT)
         self.stream.seek(0)
         self._verify_dump(json.load(self.stream))
+
+    def test_range_exception(self):
+        """An exception should be raised for a range with step != 1."""
+        with self.assertRaises(TypeError):
+            dump.Json(self.stream).run({"range": range(0, 10, 2)})
+
+class TestYamlDumper(DumperTestBase):
+    """Test YAML dumper."""
 
     def test_yaml(self):
         """Dump data in YAML format."""
         yaml_dumper = dump.Yaml(self.stream)
-        yaml_dumper.run(self._TEST_PIPELINE)
+        yaml_dumper.run(_PIPELINE_INPUT)
         self.stream.seek(0)
         self._verify_dump(yaml.safe_load(self.stream))
+
+    def test_range_exception(self):
+        """An exception should be raised for a range with step != 1."""
+        with self.assertRaises(yaml.representer.RepresenterError):
+            dump.Yaml(self.stream).run({"range": range(0, 10, 2)})
