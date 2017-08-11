ab685206a105368dcf95d986f60497b20d82424c
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-08-11 18:35:36 +0100

    Add a Map component

--- phyre_engine/test/component/test_Component.py
+++ phyre_engine/test/component/test_Component.py
@@ -1,5 +1,7 @@
 import unittest
 from phyre_engine.component import Component
+from phyre_engine.component.component import Map
+import phyre_engine.pipeline
 
 class TestComponent(unittest.TestCase):
     """Test non-abstract methods in the Component abstract base class."""
@@ -25,3 +27,52 @@ class TestComponent(unittest.TestCase):
         a, b = TestComponent.MockComponentList().get_vals(data)
         self.assertEqual(a, 123)
         self.assertEqual(b, 456)
+
+class TestMap(unittest.TestCase):
+    """Test the phyre_engine.component.Map class."""
+
+    class Double(Component):
+        """Dummy component that multiplies a number by 2."""
+        ADDS = []
+        REQUIRED = ["num"]
+        REMOVES = []
+
+        def run(self, data, config=None, pipeline=None):
+            data["num"] *= 2
+            return data
+
+    def setUp(self):
+        """Create some test data to map."""
+        self.state = {
+            "values": [
+                {"num": 1},
+                {"num": 2},
+                {"num": 3},
+            ]
+        }
+        self.expected_state = {
+            "values": [
+                {"num": 2},
+                {"num": 4},
+                {"num": 6},
+            ]
+        }
+
+    def test_map_pipeline(self):
+        """Run Map with Pipeline as input."""
+        components = [self.Double()]
+        pipeline = phyre_engine.pipeline.Pipeline(components)
+        map_cmpt = Map("values", pipeline)
+
+        results = map_cmpt.run(self.state)
+        self.assertDictEqual(results, self.expected_state)
+
+    def test_map_load(self):
+        """Run Map with Dict as input."""
+        components = [
+            (self.Double.__module__, self.Double.__qualname__)
+        ]
+        map_cmpt = Map("values", {"components": components})
+
+        results = map_cmpt.run(self.state)
+        self.assertDictEqual(results, self.expected_state)
