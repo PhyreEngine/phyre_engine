422d553adb8ac6c9804d7f605606a8d1c2657dc0
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-08-18 14:23:21 +0100

    Add alter.Copy class for copying fields.

--- phyre_engine/component/alter.py
+++ phyre_engine/component/alter.py
@@ -25,3 +25,22 @@ class Remove(_AlterationComponent):
         """Remove a field from the pipeline state."""
         del data[self.field]
         return data
+
+class Copy(_AlterationComponent):
+    """
+    Make a copy of a field. A shallow copy is performed by default.
+
+    :param str src: Name of the source field.
+    :param str dst: Name of the destination field.
+    :param bool deep: Perform a deep copy.
+    """
+
+    def __init__(self, src, dst, deep=False):
+        self.src = src
+        self.dst = dst
+        self.copy_fn = copy.deepcopy if deep else copy.copy
+
+    def run(self, data, config=None, pipeline=None):
+        """Remove a field from the pipeline state."""
+        data[self.dst] = self.copy_fn(data[self.src])
+        return data
--- phyre_engine/test/component/test_alter.py
+++ phyre_engine/test/component/test_alter.py
@@ -24,3 +24,35 @@ class TestRemove(TestAlteration):
         self.assertDictEqual(
             remove.run(self.pipe_state),
             {"baz": [{"frob": 1}, 2, 3]})
+
+class TestCopy(TestAlteration):
+    """Test Copy component."""
+    _FROM = "baz"
+    _TO = "copy"
+    _EXPECTED_STATE = {
+        "foo": "bar",
+        "baz": [{"frob": 1}, 2, 3],
+        "copy": [{"frob": 1}, 2, 3]
+    }
+
+    def test_copy_shallow(self):
+        """Use Copy component to shallow copy a field."""
+        copy = alter.Copy(self._FROM, self._TO, deep=False)
+        result = copy.run(self.pipe_state)
+        self.assertDictEqual(result, self._EXPECTED_STATE)
+
+        # Altering shallow copy should alter original state
+        result["copy"][0]["frob"] = 4
+        self.assertEqual(result["baz"][0]["frob"], 4)
+        self.assertEqual(self.pipe_state["baz"][0]["frob"], 4)
+
+    def test_copy_deep(self):
+        """Use Copy component to deep copy a field."""
+        copy = alter.Copy(self._FROM, self._TO, deep=True)
+        result = copy.run(self.pipe_state)
+        self.assertDictEqual(result, self._EXPECTED_STATE)
+
+        # Altering deep copy should not alter original state
+        result["copy"][0]["frob"] = 4
+        self.assertEqual(result["baz"][0]["frob"], 1)
+        self.assertEqual(self.pipe_state["baz"][0]["frob"], 1)
