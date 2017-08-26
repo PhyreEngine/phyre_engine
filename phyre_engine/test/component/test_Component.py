0478169835139b2f94df9facc4eb22e2f27b1e2c
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-08-26 19:22:56 +0100

    Add "Conditional" component for branching pipes

--- phyre_engine/test/component/test_Component.py
+++ phyre_engine/test/component/test_Component.py
@@ -1,7 +1,18 @@
+import copy
 import unittest
 from phyre_engine.component import Component
-from phyre_engine.component.component import Map
+from phyre_engine.component.component import Map, Conditional
 import phyre_engine.pipeline
+
+class Double(Component):
+    """Dummy component that multiplies a number by 2."""
+    ADDS = []
+    REQUIRED = ["num"]
+    REMOVES = []
+
+    def run(self, data, config=None, pipeline=None):
+        data["num"] *= 2
+        return data
 
 class TestComponent(unittest.TestCase):
     """Test non-abstract methods in the Component abstract base class."""
@@ -31,16 +42,6 @@ class TestComponent(unittest.TestCase):
 class TestMap(unittest.TestCase):
     """Test the phyre_engine.component.Map class."""
 
-    class Double(Component):
-        """Dummy component that multiplies a number by 2."""
-        ADDS = []
-        REQUIRED = ["num"]
-        REMOVES = []
-
-        def run(self, data, config=None, pipeline=None):
-            data["num"] *= 2
-            return data
-
     def setUp(self):
         """Create some test data to map."""
         self.state = {
@@ -60,7 +61,7 @@ class TestMap(unittest.TestCase):
 
     def test_map_pipeline(self):
         """Run Map with Pipeline as input."""
-        components = [self.Double()]
+        components = [Double()]
         pipeline = phyre_engine.pipeline.Pipeline(components)
         map_cmpt = Map("values", pipeline)
 
@@ -69,10 +70,33 @@ class TestMap(unittest.TestCase):
 
     def test_map_load(self):
         """Run Map with Dict as input."""
-        components = [
-            (self.Double.__module__, self.Double.__qualname__)
-        ]
+        components = ["{}.{}".format(Double.__module__, Double.__qualname__)]
         map_cmpt = Map("values", {"components": components})
 
         results = map_cmpt.run(self.state)
         self.assertDictEqual(results, self.expected_state)
+
+class TestConditional(unittest.TestCase):
+    """Test the phyre_engine.component.Conditional class."""
+
+    def setUp(self):
+        """Create some test data to test on."""
+        self.states = copy.deepcopy([
+            {"num": 1, "do_double": False},
+            {"num": 2, "do_double": True}
+        ])
+
+        self.expected_states = copy.deepcopy([
+            {"num": 1, "do_double": False},
+            {"num": 4, "do_double": True}
+        ])
+
+    def test_conditional(self):
+        """Run Conditional with Pipeline as input."""
+        for start_state, end_state in zip(self.states, self.expected_states):
+            components = [Double()]
+            pipeline = phyre_engine.pipeline.Pipeline(components)
+            cond_cmpt = Conditional("do_double", pipeline)
+
+            results = cond_cmpt.run(start_state)
+            self.assertDictEqual(results, end_state)
