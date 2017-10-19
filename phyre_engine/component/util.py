9d39deaff120a01a5ee24f49dadc0182ada0f004
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2017-10-19 18:26:51 +0100

    Add MakeDir component for making directories

--- phyre_engine/component/util.py
+++ phyre_engine/component/util.py
@@ -3,7 +3,10 @@ This module contains components that do system tasks rather than bioinformatics
 tasks.
 """
 import os
+from pathlib import Path
+
 from phyre_engine.component.component import Component
+
 
 class ChangeDir(Component):
     """
@@ -18,4 +21,28 @@ class ChangeDir(Component):
         """Change directory to ``working_dir``."""
         working_dir = self.get_vals(data)
         os.chdir(working_dir)
+        return data
+
+class MakeDir(Component):
+    """
+    Make a new directory. The new directory must be given in the ``working_dir``
+    key of the pipeline state.
+
+    :param bool exist_ok: If `False`, an error will be raised if the target
+        directory already exists.
+
+    :param bool parents: If `True`, create all parents of the directory.
+    """
+    REQUIRED = ["working_dir"]
+    ADDS = []
+    REMOVES = []
+
+    def __init__(self, exist_ok=True, parents=True):
+        self.exist_ok = exist_ok
+        self.parents = parents
+
+    def run(self, data, config=None, pipeline=None):
+        """Create a new directory."""
+        working_dir = self.get_vals(data)
+        Path(working_dir).mkdir(parents=self.parents, exist_ok=self.exist_ok)
         return data
--- phyre_engine/test/component/test_util.py
+++ phyre_engine/test/component/test_util.py
@@ -4,6 +4,8 @@ import pathlib
 import tempfile
 import unittest
 import phyre_engine.component.util as util
+import shutil
+from pathlib import Path
 
 class TestChangeDir(unittest.TestCase):
     """Test the ChangeDir component."""
@@ -20,3 +22,46 @@ class TestChangeDir(unittest.TestCase):
                     pathlib.Path(tempdir).resolve())
         finally:
             os.chdir(orig_dir)
+
+class TestMakeDir(unittest.TestCase):
+    """Test the MakeDir component."""
+
+    def setUp(self):
+        """Create tmpdir for testing."""
+        self.tmpdir = tempfile.mkdtemp("-test", "mkdir-")
+
+    def tearDown(self):
+        """Remoev tmpdir."""
+        shutil.rmtree(self.tmpdir)
+
+    def test_mkdir_parents(self):
+        """Create a nested subdirectory."""
+        mkdir = util.MakeDir(exist_ok=True, parents=True)
+        target = Path(self.tmpdir, "a/b/c")
+        mkdir.run({"working_dir": target})
+        self.assertTrue(target.exists(), "Created {}".format(target))
+
+    def test_mkdir_no_exist(self):
+        """Cannot create a directory that already exists when exist_ok=False."""
+        mkdir = util.MakeDir(exist_ok=False, parents=True)
+
+        target = Path(self.tmpdir, "a/b/c")
+        mkdir.run({"working_dir": target})
+        self.assertTrue(target.exists(), "Created {}".format(target))
+
+        with self.assertRaises(FileExistsError):
+            mkdir.run({"working_dir": target})
+
+    def test_mkdir_no_parents(self):
+        """Creating a nested subdirectory should fail with parents=False."""
+        mkdir = util.MakeDir(exist_ok=True, parents=False)
+
+        # Nested creation fails
+        with self.assertRaises(FileNotFoundError):
+            target = Path(self.tmpdir, "a/b/c")
+            mkdir.run({"working_dir": target})
+
+        # Non-nested works
+        target = Path(self.tmpdir, "a")
+        mkdir.run({"working_dir": target})
+        self.assertTrue(target.exists(), "Created {}".format(target))
